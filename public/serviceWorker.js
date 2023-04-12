d3a06687cb8667b09fd97bc891e0e69649931ad5
Austin Smith <asmit2222@gmail.com>
2023-04-12 14:47:00 -0400

    nm

---
+++ public/serviceWorker.js
@@ -0,0 +1,27 @@
+let CACHE_NAME = "my-site-cache-v1";
+const urlsToCache = ["/", "/index.html"];
+
+self.addEventListener("install", function (event) {
+  caches.keys().then(function (keys) {
+    keys.forEach(function (key) {
+      console.log("CacheKey: " + key);
+      if (key !== CACHE_NAME) {
+        caches.delete(key).then(function (deleted) {
+          if (deleted) {
+            console.log(key + " deleted successfully");
+          } else {
+            console.error(key + " not found");
+          }
+        });
+      }
+    });
+  });
+
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Cache opened:", CACHE_NAME);
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
